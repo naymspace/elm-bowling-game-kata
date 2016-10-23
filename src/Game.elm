b2cd7e1d5d2fcfc00b958a2793eb54f68c64e86a
Pierre Beitz <pb@naymspace.de>
2016-10-23 17:33:09 +0200

    a solution

--- src/Game.elm
+++ src/Game.elm
@@ -10,5 +10,39 @@ type alias Score =
 
 
 score : Game -> Score
-score game =
-    -1
+score =
+    reduce >> List.take 10 >> List.sum
+
+
+scoreFrame : Game -> ( Score, Game )
+scoreFrame rolls =
+    case rolls of
+        x1 :: x2 :: x3 :: xs ->
+            if x1 == 10 then
+                ( x1 + x2 + x3, x2 :: x3 :: xs )
+            else if x1 + x2 == 10 then
+                ( x1 + x2 + x3, x3 :: xs )
+            else
+                ( x1 + x2, x3 :: xs )
+
+        {- last two throws of a normal game -}
+        [ x1, x2 ] ->
+            ( x1 + x2, [] )
+
+        [ x ] ->
+            ( x, [] )
+
+        [] ->
+            ( 0, [] )
+
+
+reduce : Game -> List Score
+reduce rolls =
+    let
+        ( frameScore, remainingRolls ) =
+            scoreFrame rolls
+    in
+        if rolls == [] then
+            []
+        else
+            frameScore :: reduce remainingRolls
